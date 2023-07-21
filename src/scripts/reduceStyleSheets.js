fad2269bdb7b7b520e245a8dee80a28b6af59214
Naman Jain <naman.jain1@sprinklr.com>
2023-07-21 16:00:02 +0530

    final push

--- src/scripts/reduceStyleSheets.js
+++ src/scripts/reduceStyleSheets.js
@@ -2,6 +2,6 @@ import { wrapper } from "./package.js";
 import path from "path";
 
 
-let dir = "../../testinng-repos/space-tourism";
+let dir = "../../testinng-repos/netflix-clone";
 dir = path.resolve(dir);
 wrapper(dir);
